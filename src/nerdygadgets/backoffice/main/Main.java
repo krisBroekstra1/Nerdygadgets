1552ef8d76619d23560d9f839d94e5e0015c1f24
Juliën <julien.kolkman@gmail.com>
2020-04-17 11:46:50 +0200

    Update Main.java

--- src/nerdygadgets/backoffice/main/Main.java
+++ src/nerdygadgets/backoffice/main/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String args[]){
         System.out.println("Hoi dit is een test.");
         System.out.println("Hoi dit is een tes2t.");
+        System.out.println("Hoi dit is een tes2t.");
     }
 }

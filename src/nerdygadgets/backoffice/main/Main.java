4f8ce6a9716422b0e4cf50b1c0e7e4a84c39013d
Kris Broekstra <kris.broekstra@outlook.com>
2020-04-17 11:49:11 +0200

    potentiele fix

--- src/nerdygadgets/backoffice/main/Main.java
+++ src/nerdygadgets/backoffice/main/Main.java
@@ -1,8 +1,8 @@
-package nerdygadgets.backoffice.main;
 
 public class Main {
     public static void main(String[] args){
         System.out.println("wollah nerdygadgets");
-        System.out.println("Tantu dikke neger");
+        System.out.println("10000 producten verkocht");
+        System.out.println("huts");
     }
 }

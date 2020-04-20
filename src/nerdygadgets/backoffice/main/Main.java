8fd5df864a6cc817c0e2c34d48f2c0931d6b08fe
JustinGlaser <57943544+JustinGlaser@users.noreply.github.com>
2020-04-20 09:06:01 +0200

    Test

--- src/nerdygadgets/backoffice/main/Main.java
+++ src/nerdygadgets/backoffice/main/Main.java
@@ -2,9 +2,7 @@ package nerdygadgets.backoffice.main;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("wollah nerdygadgets");
-        System.out.println("10000 producten verkocht");
-        System.out.println("huts");
-        System.out.println("wollah");
+        System.out.println("Werkt ie zo bij iedereen?");
+        System.out.println("Of niet?");
     }
 }

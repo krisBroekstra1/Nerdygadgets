e5166c609f4546bed3211a5843dd530b6c7d8502
IngvarSchoenmaker <ingvar@brownfox.nl>
2020-04-20 09:34:24 +0200

    Test of het werkt

--- src/nerdygadgets/backoffice/main/Main.java
+++ src/nerdygadgets/backoffice/main/Main.java
@@ -3,6 +3,6 @@ package nerdygadgets.backoffice.main;
 public class Main {
     public static void main(String[] args){
         System.out.println("Werkt ie zo bij iedereen?");
-        System.out.println("Of niet?");
+        System.out.println("Of niet? test");
     }
 }

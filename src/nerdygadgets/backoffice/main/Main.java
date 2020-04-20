a68018a874b7d115e0abfc63bd8315f114153317
Kris Broekstra <kris.broekstra@outlook.com>
2020-04-20 09:03:38 +0200

    test

--- src/nerdygadgets/backoffice/main/Main.java
+++ src/nerdygadgets/backoffice/main/Main.java
@@ -1,8 +1,10 @@
+package nerdygadgets.backoffice.main;
 
 public class Main {
     public static void main(String[] args){
         System.out.println("wollah nerdygadgets");
         System.out.println("10000 producten verkocht");
         System.out.println("huts");
+        System.out.println("wollah");
     }
 }

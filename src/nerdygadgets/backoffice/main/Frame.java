0ca156853256e75510d7bb15560d5725465ce1a1
joeyICTMb2 <58032086+joeyICTMb2@users.noreply.github.com>
2020-04-21 09:08:11 +0200

    Update Frame.java

--- src/nerdygadgets/backoffice/main/Frame.java
+++ src/nerdygadgets/backoffice/main/Frame.java
@@ -1,16 +1,29 @@
 package nerdygadgets.backoffice.main;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Frame extends JFrame {
+public class Frame extends JFrame implements ActionListener {
+    private JLabel _label;
     public Frame (int width, int height){
+        setLayout(new BorderLayout());
         setSize(width, height);
         setTitle("NerdyGadgets backoffice");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        _label = new JLabel("orders");
+        add(_label);
+
 
         setVisible(true);
         repaint();
 
     }
 
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }

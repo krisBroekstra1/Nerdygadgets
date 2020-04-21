1e3c61711d522c4a90d886beb6efb4eb8d1e4833
JustinGlaser <57943544+JustinGlaser@users.noreply.github.com>
2020-04-21 12:14:05 +0200

    Halverwege met functies

--- src/nerdygadgets/backoffice/main/JDBC/Driver.java
+++ src/nerdygadgets/backoffice/main/JDBC/Driver.java
@@ -1,16 +1,22 @@
 package nerdygadgets.backoffice.main.JDBC;
+
 import java.sql.*;
+
 public class Driver {
     public static void main(String[] args) {
+        adressen();
+    }
+
+
+    public static ResultSet adressen() {
         try {
-            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost/wideworldimporters", "elke", "");
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost/wideworldimporters", "root", "");
             Statement myStmt = myConn.createStatement();
             ResultSet myRs = myStmt.executeQuery("SELECT DeliveryInstructions FROM invoices");
-            while (myRs.next()){
-                System.out.println(myRs.getString("DeliveryInstructions"));
-            }
-        } catch (Exception e){
+            return myRs;
+        } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
     }
 }

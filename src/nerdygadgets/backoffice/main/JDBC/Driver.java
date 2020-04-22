33db73ab7a2ee73df851ca2b628512beeb7d23de
JustinGlaser <57943544+JustinGlaser@users.noreply.github.com>
2020-04-22 09:25:59 +0200

    Functies met while

--- src/nerdygadgets/backoffice/main/JDBC/Driver.java
+++ src/nerdygadgets/backoffice/main/JDBC/Driver.java
@@ -4,10 +4,27 @@ import java.sql.*;
 
 public class Driver {
     public static void main(String[] args) {
-        adressen();
+        medewerkers();
     }
 
 
+
+//Functies
+    //Test functie
+    public static void functie() {
+        try {
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost/wideworldimporters", "root", "");
+            Statement myStmt = myConn.createStatement();
+            ResultSet myRs = myStmt.executeQuery("Vul hier jouw SQL query in!");
+            while (myRs.next()){
+                System.out.println(myRs.getString("Vul hier je columnLabel in!"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Functie adressen
     public static void adressen() {
         try {
             Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost/wideworldimporters", "root", "");
@@ -15,6 +32,20 @@ public class Driver {
             ResultSet myRs = myStmt.executeQuery("SELECT DeliveryInstructions FROM invoices");
             while (myRs.next()){
                 System.out.println(myRs.getString("DeliveryInstructions"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Functie medewerkers
+    public static void medewerkers() {
+        try {
+            Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost/wideworldimporters", "root", "");
+            Statement myStmt = myConn.createStatement();
+            ResultSet myRs = myStmt.executeQuery("SELECT FullName FROM people");
+            while (myRs.next()){
+                System.out.println(myRs.getString("FullName"));
             }
         } catch (Exception e) {
             e.printStackTrace();

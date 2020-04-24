f58000bda5706f146b0b0bd2c19f22cb4c3fe557
JustinGlaser <57943544+JustinGlaser@users.noreply.github.com>
2020-04-24 09:14:25 +0200

    Layout code

--- src/nerdygadgets/backoffice/main/Frame.java
+++ src/nerdygadgets/backoffice/main/Frame.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
-public class Frame extends JFrame{
+public class Frame extends JFrame {
     JButton JBopenGPSPanel;
     private Gpscoördinate Gpscoördinate;
 
